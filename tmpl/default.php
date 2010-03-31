4208705a2d0aaa34deadc13996ba3608edad778c
Sylvain Philip <contact@sphilip.com>
2010-03-31 10:03:47 +0000

    Update licences, and svn properties

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,4 +1,19 @@
-<?php // no direct access
+<?php
+/**
+ * This file is part of Joomla Estate Agency - Joomla! extension for real estate agency
+ * 
+ * @version     $Id$
+ * @package		Jea.module.emphasis
+ * @copyright	Copyright (C) 2008 PHILIP Sylvain. All rights reserved.
+ * @license		GNU/GPL, see LICENSE.txt
+ * Joomla Estate Agency is free software. This version may have been modified pursuant to the
+ * GNU General Public License, and as distributed it includes or is derivative
+ * of works licensed under the GNU General Public License or other free or open
+ * source software licenses.
+ * 
+ */
+
+// no direct access
 defined('_JEXEC') or die('Restricted access');
 
 JHTML::stylesheet('mod_jea_emphasis.css', 'modules/mod_jea_emphasis/');
