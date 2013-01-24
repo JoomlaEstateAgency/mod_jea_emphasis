e8d1f98db29645d3eeb3fad099a2807d0d998ee2
Sylvain Philip <contact@sphilip.com>
2013-01-24 22:30:49 +0000

    Fix install error

--- script.php
+++ script.php
@@ -24,8 +24,9 @@ class mod_jea_emphasisInstallerScript
         $jea = JFactory::getXML(JPATH_ROOT.'/administrator/components/com_jea/jea.xml');
         if ((float) $jea->version < 2.2) {
             JError::raiseWarning(500, 'JEA version must be >= 2.2. Please upgrade before JEA component');
+            return false;
         }
-        return false;
+        return true;
     }
 
 
